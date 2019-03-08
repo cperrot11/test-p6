372c323923454644a9ce375352018027e7f8460b
c.perrotin <christophe@perrotin.eu>
2019-03-08 13:49:55 +0100

    Entity Comment

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Comment;
 use App\Form\ArticleType;
+use App\Form\CommentType;
 use App\Repository\ArticleRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Request;
@@ -65,10 +67,24 @@ class MainController extends AbstractController
     /**
      * @Route("/trick/{id}", name="show_trick")
      */
-    public function showTrick(Article $article){
+    public function showTrick(Article $article, Request $request, ObjectManager $manager){
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $comment->setCreatedAt(new \DateTime())
+                    ->setArticle($article);
+
+            $manager->persist($comment);
+            $manager->flush();
+        }
+
         return $this->render('article/show.html.twig',[
             'title'=>'Trick',
-            'article'=>$article
+            'article'=>$article,
+            'commentForm'=> $form->createView()
         ]);
     }
 }
